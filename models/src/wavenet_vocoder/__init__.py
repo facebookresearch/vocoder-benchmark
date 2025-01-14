2b81cf8c91ddecd241e71f91657649aae828012a
Lucas Kabela <lucaskabela@meta.com>
2025-01-14 15:31:33 -0800

    Remove unused python imports
    Summary: See title; we run codemod to removeu unused imports then lint
    
    Reviewed By: liujun77
    
    Differential Revision: D67984934
    
    fbshipit-source-id: 5514e986e52a5f874708839d18823f267d096104

--- models/src/wavenet_vocoder/__init__.py
+++ models/src/wavenet_vocoder/__init__.py
@@ -3,14 +3,3 @@
 # pyre-strict
 
 from __future__ import absolute_import, print_function, with_statement
-
-from models.src.wavenet_vocoder.version import ( # @oss-only
-# @fb-only: from langtech.tts.vocoders.models.src.wavenet_vocoder.version import ( 
-    version as __version__,
-)
-
-from models.src.wavenet_vocoder.wavenet import ( # @oss-only
-# @fb-only: from langtech.tts.vocoders.models.src.wavenet_vocoder.wavenet import ( 
-    receptive_field_size,
-    WaveNet,
-)
